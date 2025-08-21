f5d3cf0f50240ebe10efbc2f880f26ee883962dc
Agnan Toro <agnankun18@gmail.com>
2025-08-22 01:54:08 +0700

    add image-uploader.vue placeholder

---
+++ resources/js/components/image-uploader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="image-uploader">
+    <input type="file" />
+    <p style="color: #888; font-size: 14px;">Image Uploader Placeholder</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageUploader',
+};
+</script>
+
+<style scoped>
+.image-uploader {
+  border: 1px dashed #ccc;
+  padding: 16px;
+  border-radius: 8px;
+  text-align: center;
+}
+</style>
